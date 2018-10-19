ae24c83fb94802f4465e8644cdb33778e04124ca
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-10-19 16:45:02 +0200

    Added old and new projects

---
+++ Pinball NEW/ModuleSceneIntro.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Module.h"
+#include "p2List.h"
+#include "p2Point.h"
+#include "Globals.h"
+
+class PhysBody;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+	void OnCollision(PhysBody* bodyA, PhysBody* bodyB);
+	void AddStaticBodies();
+
+public:
+	//PhysBodies==================================
+	p2List<PhysBody*> circles;
+	p2List<PhysBody*> boxes;
+	p2List<PhysBody*> chains;
+
+	//Textures====================================
+	SDL_Texture* ball;
+	SDL_Texture* pinball;
+	SDL_Texture* circle;
+
+	//Sfx=========================================
+
+
+
+	//Delete------------------------
+	SDL_Texture* box;
+	SDL_Texture* rick;
+	uint bonus_fx;
+};
---
+++ Pinball OLD/README.md
@@ -0,0 +1,33 @@
+# Version of the game
+
+Physics4Pinball v.0.0.1.
+
+## Description
+
+In this version of the game you will be able to place a ball on the pinball and see how it collides.
+
+## Links
+Lluis Moreu https://github.com/youis11 
+
+Alejandro Gamarra https://github.com/alejandro61299
+
+Github repository: https://github.com/PowerHunters/Physics4Pinball
+
+Github organization: https://github.com/PowerHunters
+
+Changelist: https://github.com/PowerHunters/Physics4Pinball/blob/master/CHANGELOG.md
+
+## Controls
+
+
+Debug mode:
+
+1 - Place a ball on the mouse position
+2 - Place a ball on the start position
+3 - Place a ball on the top of the pinball
+
+F1 - Show/hide collision boxes
+
+Can you finish the game?
+
+We hope you Enjoy! Thanks for playing.

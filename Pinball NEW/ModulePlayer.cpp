59268ba1f2ea46c528419a446eb2541f835afacf
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-10-21 13:53:42 +0200

    Fixed barrier bug

--- Pinball NEW/ModulePlayer.cpp
+++ Pinball NEW/ModulePlayer.cpp
@@ -164,7 +164,11 @@ update_status ModulePlayer::Update()
 
 	//--------Barrier----------------------------------------------
 	if (create_barrier == true)
-		barrier = App->physics->CreateRectangle(397, 139, 33, 3, -60, true);
+	{
+		barrier = App->physics->CreateRectangle(397, 139, 33, 3, -60, false);
+		create_barrier = false;
+	}
+		
 
 	return UPDATE_CONTINUE;
 }
